43ba28627598c90bb0b5f9fe5bcf97b8b89f9e1d
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-08-06 21:54:39 +0700

    New Commit

--- src/enumerate/VendingMachine.java
+++ src/enumerate/VendingMachine.java
@@ -29,4 +29,43 @@ enum Category {
     }
 }
 public class VendingMachine {
+    private static State state = State.RESTING;
+    private static int amount = 0;
+    private static Input selection = null;
+    enum StateDuration {TRANSIENT}
+    enum State {
+        RESTING {
+            public void next(Input input) {
+                switch (Category.categozie(input)) {
+                    case MONEY:
+                        amount += input.amount();
+                        state = State.ADDING_MONEY;
+                        break;
+                    case SHUTDOWN:
+                        state = State.TERMINAL;
+                    default:
+                }
+            }
+        },
+        ADDING_MONEY {
+            public void next(Input input) {
+                switch
+            }
+        },
+        DISPENSING {
+
+        },
+        GIVING_CHANGE {
+
+        },
+        TERMINAL {
+
+        };
+        private boolean isTransaction = false;
+        void next(Input input) {
+            throw new RuntimeException("Only call next() method for " +
+            "StateDuration.Transaction state");
+        }
+        void output() {System.out.println(amount);}
+    }
 }
